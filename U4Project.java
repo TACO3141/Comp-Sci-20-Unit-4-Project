cd4613dd25fabb2774ff1316b591e719a4fb4a7f
TACO3141 <andrewej10@gmail.com>
2025-11-14 11:42:29 -0700

    Create U4Project.java
    And some work on it

---
+++ U4Project.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+class U4Project
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Exception Caught: Matrix.Backend.Main.UserNotRecognizedException: Biometric failure. Manual Override Intiated.\n...");
+    try
+      {
+        TimeUnit.SECONDS.sleep(3);
+      }
+    catch(InterruptedException e)
+      {
+      }
+    System.out.println("Override Successful. Welcome, administrator. Data corrupted. Enter new password:")
+    String password = scan.nextLine();
+    System.out.println("Password contains " +  password.length() + " characters.");
+    System.out.println("Scramble initiated.");
+    for (int )
+  }
+}
